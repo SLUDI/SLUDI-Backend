670d2d142393c385be9e5e185bed261f828806ce
Bhashithajkak <kavindubhashitha2000@gmail.com>
2025-10-24 07:59:12 +0530

    refactor(exception): update global exception handler

--- src/main/java/org/example/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/exception/GlobalExceptionHandler.java
@@ -1,10 +1,17 @@
 package org.example.exception;
 
+import com.google.protobuf.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.example.dto.ApiResponseDto;
 import org.example.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.Instant;
+
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -17,5 +24,19 @@ public class GlobalExceptionHandler {
                 .build();
         
         return ResponseEntity.badRequest().body(error);
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponseDto<Void>> handleGeneralException(Exception ex){
+        log.error("Unexpected error",ex);
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ApiResponseDto.<Void>builder()
+                        .success(false)
+                        .errorCode("INTERNAL ERROR")
+                        .message("Unexpected server error")
+                        .timestamp(Instant.now())
+                        .build()
+                );
+
     }
 }
